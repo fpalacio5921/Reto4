08607f6df979204f7be89a7772067b0a1f18e50b
fpalacio5921 <fpalacio5921@gmail.com>
2021-10-30 14:41:06 -0500

    Alistamiento de Proyecto para modulo de Seguridad

---
+++ src/main/java/Reto4/Reto4/Servicios/SecurityAdapter.java
@@ -0,0 +1,25 @@
+package Reto4.Reto4.Servicios;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+public class SecurityAdapter extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests(a -> a
+                .antMatchers("/", "/error", "/webjars/**",
+                        "/api/**").permitAll()
+                .anyRequest().authenticated()
+        )
+                .exceptionHandling(e -> e
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
+        )
+                .oauth2Login().defaultSuccessUrl("/", true);
+                 http.cors().and().csrf().disable();
+
+        }
+
+}
